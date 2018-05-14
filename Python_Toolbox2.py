582d0e4ddc1499e231c1c3a55348d8d0a794acf6
Soonyoung Jeoung <tnsdud1688@gmail.com>
2018-05-14 01:15:40 -0700

    Update Python_Toolbox2.py

--- Python_Toolbox2.py
+++ Python_Toolbox2.py
@@ -1,3 +1,19 @@
 # Create list comprehension: squares
 squares = [i**2 for i in range(0,10)]
 
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+# Print the new list
+print(new_fellowship)
+
