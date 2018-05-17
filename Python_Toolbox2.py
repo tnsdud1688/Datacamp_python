128289ebd2baf41e41318abc9f8e5761dcb353f1
Soonyoung Jeoung <tnsdud1688@gmail.com>
2018-05-17 01:21:01 -0700

    Update Python_Toolbox2.py

--- Python_Toolbox2.py
+++ Python_Toolbox2.py
@@ -17,3 +17,11 @@ new_fellowship = [member for member in fellowship if len(member) >= 7]
 # Print the new list
 print(new_fellowship)
 
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member) >= 7 else '' for member in fellowship]
+
+# Print the new list
+print(new_fellowship)
