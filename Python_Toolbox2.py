b67692a343a41571e3ee93d042fac799880273c8
Soonyoung Jeoung <tnsdud1688@gmail.com>
2018-06-28 00:33:40 -0700

    Update Python_Toolbox2.py

--- Python_Toolbox2.py
+++ Python_Toolbox2.py
@@ -63,3 +63,23 @@ rs_fxn = lists2dict(feature_names, row_vals)
 
 # Print rs_fxn
 print(rs_fxn)
+
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+
+    # Iterate over the generator from read_large_file()
+    for line in read_large_file(file):
+
+        row = line.split(',')
+        first_col = row[0]
+
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+        else:
+            counts_dict[first_col] = 1
+
+# Print            
+print(counts_dict)
