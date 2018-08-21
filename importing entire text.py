533433e748b519ab37114af938fa3f66be8b7649
Soonyoung Jeoung <tnsdud1688@gmail.com>
2018-08-21 12:48:25 -0700

    Update importing entire text.py

--- importing entire text.py
+++ importing entire text.py
@@ -66,3 +66,12 @@ plt.xlabel('time (min.)')
 plt.ylabel('percentage of larvae')
 plt.show()
 
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, delimiter = ',', names=True, dtype=None)
+
+# Print out first three entries of d
+print(d[:3])
+
